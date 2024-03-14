d1f50fcdb55cdefc16b6861e7bb0ac4a169bae40
Raaz <21ketanraaz@gmail.com>
2024-03-14 13:13:53 +0530

    Updated Codeforces userDetails

--- src/Components/Main/Main-Content/CodeForces/UserDetail.jsx
+++ src/Components/Main/Main-Content/CodeForces/UserDetail.jsx
@@ -3,14 +3,16 @@
 
 const UserProfile = ({ user }) => {
  if (!user) return <p>Loading...</p>;
-console.log(user.result[0].rating);
+const userDetails = user.result[0];
+console.log(userDetails);
  return (
     <div >
       <div >
-        {/* <h2 >Username: {user.handle}</h2> Updated property name */}
-        <p >Rank: {user.rank}</p> {/* Updated property name */}
-        <p >Rating: {user.rating}</p> {/* New property */}
-        <p >Max Rating: {user.maxRating}</p> {/* New property */}
+        <h3 >Username: {userDetails.handle}</h3> Updated property name
+        <p >Rating: {userDetails.maxRating}</p> {/* New property */}
+        <p >Rank: {userDetails.rank}</p> {/* Updated property name */}
+        <p >Contribution: {userDetails.contribution}</p> {/* New property */}
+        <img src={userDetails.avatar}></img>
         {/* Display other details as needed */}
       </div>
     </div>
