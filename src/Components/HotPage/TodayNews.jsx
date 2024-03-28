47b04093cac9dfcdea94785e3e22d136aeb4d0a3
Raaz <21ketanraaz@gmail.com>
2024-03-28 20:16:59 +0530

    Working with todays News

--- src/Components/HotPage/TodayNews.jsx
+++ src/Components/HotPage/TodayNews.jsx
@@ -1,8 +1,36 @@
+import styles from './TodayNews.module.css';
 function TodaysNews() {
   return (
-    <div>
-      <h1>Today's News</h1>
-      {/* <News /> */}
+    <div className={styles.main}>
+      <h1>TodaysNews</h1>
+      <section>
+        <span>
+          Categories
+        </span>
+        <span>
+          <button>Technology</button>
+          <button>Entertainment</button>
+          <button>Science</button>
+          <button>Health</button>
+          <button>Sports</button>
+          <button>Business</button>
+          <button>General</button>
+          <button>Politics</button>
+          <button>Education</button>
+          <button>Automobile</button>
+        </span>
+      </section>
+      <section>
+        <span>
+          Sort By
+        </span>
+        <span>
+          <button>Latest</button>
+          <button>Popular</button>
+        </span>
+      </section>
+      
+      
     </div>
   );
 }
