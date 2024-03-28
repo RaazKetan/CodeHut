520419965bd85c1b63df0662b5e5b03a3e971351
Raaz <21ketanraaz@gmail.com>
2024-03-28 19:31:40 +0530

    Working With Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Header from './Components/Header/Header'
 import HeroSection from './Components/HeroSection/HeroSection'
 import './App.css'
 import Main from './Components/Main/Main'
-
+import { Route, Routes } from 'react-router-dom'
 // import express from 'express';
 
 function App() {
   return (
     <>
-      <Header/>
-      <HeroSection/>
-      <Main/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
     </>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,24 +10,24 @@ const Header = () => {
         <nav className={classes["nav-bar"]}>
         <ul>
           <li>
-          <Link to="/codee">Codee {'</>'}</Link>
+          <Link>Codee {'</>'}</Link>
             {/* <a href="#">Codee {'</>'}</a> */}
           </li>
           <li>
-          <Link to={'/home'}>Home</Link>
+          <Link>Home</Link>
           </li>
           <li>
           <a href="https://github.com/RaazKetan/CodeHut" target="_blank" rel="noopener noreferrer">Get Code</a>
 
           </li>
           <li>
-          <Link to={'/about'}>About Me</Link>
+          <Link >About Me</Link>
           </li>
           <li>
             <a href="https://ketan-raj-2-0.vercel.app/" target='_black'>Other Work</a>
           </li>
           <li>
-          <Link to="#main" onClick={check}>Get Started</Link>
+          <Link  onClick={check}>Get Started</Link>
           </li>
         </ul>
       </nav>
