a45934966861d51f6c6764690007b8068c85543e
Raaz <21ketanraaz@gmail.com>
2024-04-15 18:41:30 +0530

    Modifying Git Calender

--- src/Components/HotPage/GitCalender.jsx
+++ src/Components/HotPage/GitCalender.jsx
@@ -6,11 +6,23 @@ import GitHubCalendar from 'react-github-calendar';
 
 
 function GitCalender() {
-
+const [username, setUsername] = useState('raazketan');
  return (
     <div className={styles.main}>
       <h1>GitHub Contributions Calendar</h1>
-      <GitHubCalendar username="raazketan" blockSize={15} blockMargin={5} />
+      <div className={styles.search}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter your GitHub username"/>
+        <button onClick={() => setUsername(username)}>Submit</button>
+        </div>
+      <GitHubCalendar username={username} 
+      colorScheme='light'
+      blockRadius={20}
+      year={2020}
+      blockSize={15} blockMargin={5} />
    
     </div>
  );
